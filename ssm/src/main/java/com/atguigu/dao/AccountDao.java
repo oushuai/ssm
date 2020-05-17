909d474c1962833c918d58be1e86447a0c94d42f
oushuai <2313968604@qq.com>
2020-05-18 07:13:35 +0800

    accountDao删除没用的dao注解

--- ssm/src/main/java/com/atguigu/dao/AccountDao.java
+++ ssm/src/main/java/com/atguigu/dao/AccountDao.java
@@ -3,11 +3,8 @@ package com.atguigu.dao;
 import com.atguigu.domain.Account;
 import org.apache.ibatis.annotations.Insert;
 import org.apache.ibatis.annotations.Select;
-import org.springframework.stereotype.Repository;
 
 import java.util.List;
-//@Repository("accountDao1")
-@Repository
 public interface AccountDao {
 
     //mybtis提供代理对象不需要写实现类
